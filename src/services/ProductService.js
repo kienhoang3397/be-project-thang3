0e02233af672a9a71df7c9d400b9db3f1f84f8c4
kienhoang3397 <125807253+kienhoang3397@users.noreply.github.com>
2024-05-12 13:32:21 +0700

    first commit

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -30,7 +30,7 @@ const createProduct = async (newProduct) => {
             description,
             sold,
             discountStartDate,
-            discountEndDate
+            discountEndDate,
         });
 
         return {
